db76d90cde14ceeba4969233bb684d4047270388
badesahebmaruf <bademaruf123@gmail.com>
2023-05-17 13:54:51 +0530

    Your commit message here

--- client/src/components/SigningComponents/Register.js
+++ client/src/components/SigningComponents/Register.js
@@ -1,9 +1,82 @@
-import React from 'react'
+import React, { useState } from 'react';
+
 
 function Register() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [contactNumber, setContactNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleNameChange = (e) => {
+    setName(e.target.value);
+  };
+
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handleContactNumberChange = (e) => {
+    setContactNumber(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Handle form submission, e.g., make an API call to create a new user
+  };
   return (
-    <div>Register</div>
-  )
+    <div>
+      <div>Register Account</div>
+      <form onSubmit={handleSubmit} className='content'>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={handleNameChange} />
+        </label>
+        <br />
+        <label>
+          Email Address:
+          <input type="email" value={email} onChange={handleEmailChange} />
+        </label>
+        <br />
+        <label>
+          Contact Number:
+          <input
+            type="text"
+            value={contactNumber}
+            onChange={handleContactNumberChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <br />
+        <label>
+          Confirm Password:
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Register</button>
+      </form>
+      </div>
+    )
 }
 
 export default Register
