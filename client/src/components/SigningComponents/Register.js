b6fe0fc7dbe07770c90d1fd02d3c04452c2bea9e
Abilash-123 <abilashabi9676@gmail.com>
2023-05-19 09:32:25 +0530

    [Added]: implemented createproposal page and modifications did in register page

--- client/src/components/SigningComponents/Register.js
+++ client/src/components/SigningComponents/Register.js
@@ -8,7 +8,7 @@ function Register({setLog}) {
   const [contactNumber, setContactNumber] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
-  const [resetAnswer, setresetAnswer] = useState('');
+  const [resetAnswer, setResetAnswer] = useState('');
 
   const handleNameChange = (e) => {
     setName(e.target.value);
@@ -31,7 +31,7 @@ function Register({setLog}) {
   };
 
   const handleresetAnswer = (e) => {
-    setPassword(e.target.value);
+    setResetAnswer(e.target.value);
   };
 
   const handleSubmit = (e) => {
